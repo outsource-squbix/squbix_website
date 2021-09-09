4c0ff783acd4501d008b76c2501336d70aa9c2cd
Umang Tiwari <umang.tiwari@squbix.com>
2021-09-09 13:56:34 +0530

    added 404 page

--- src/Certificate_Verification/Error.js
+++ src/Certificate_Verification/Error.js
@@ -1,17 +1,31 @@
 import React from "react";
-
+import styled from "styled-components";
 const Error = () => {
   return (
     <div>
+      <Header className="text-center">
       <div
         class="display-5"
         style={{ color: "red", textAlign: "center", marginTop: "50vh" }}
       >
         Invalid Data. Certificate does not exist
       </div>
-      ;
+      </Header>
+      
     </div>
   );
 };
 
 export default Error;
+const Header = styled.div`
+  margin-top: 200px;
+  margin-bottom: 150px;
+  position: relative;
+  width: 50%;
+  left: 50%;
+  transform: translate(-50%);
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
